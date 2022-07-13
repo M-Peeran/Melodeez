295c3e7dc9e1096a42d6c4f888974a98fcc878da
M-Peeran <peeramakrabbi@gmail.com>
2022-07-13 20:53:24 +0530

    Refactored logic of toggleTrackSelection, addTracksToPlaylist, clearSelection and removed unwanted event class

--- app/src/main/java/com/peeranm/melodeez/feature_music_playback/presentation/tracks_by_playlist/add_track/Event.kt
+++
@@ -1,8 +0,0 @@
-package com.peeranm.melodeez.feature_music_playback.presentation.tracks_by_playlist.add_track
-
-
-sealed class Event {
-    object Clear : Event()
-    class AddSelectedTracksToPlaylist(val playlistId: Long) : Event()
-    class ToggleTrackSelection(val trackId: Long, val isSelected: Boolean) : Event()
-}
